aecb903be4e3498c00ab78a00192d70f1652723f
James Traver <jtraverjr@gmail.com>
2016-05-08 18:48:03 -0500

    firebase init

--- app/src/main/java/com/fireside/codeforcoffee/firesidechat/MainActivity.java
+++ app/src/main/java/com/fireside/codeforcoffee/firesidechat/MainActivity.java
@@ -13,12 +13,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.firebase.client.Firebase;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Firebase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Firebase.setAndroidContext(this);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -40,6 +45,9 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        db = new Firebase("https://firechat-android-db.firebaseio.com/");
+
     }
 
     @Override
